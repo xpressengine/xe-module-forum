b1386f66b58eaec83dc8ed4b4f0e15cd70bb7f3e
dragan-dan <ddan.dragan@gmail.com>
2012-03-01 16:24:22 +0000

    Issue 129:	Hint for category creation
    git-svn-id: http://xe-forum.googlecode.com/svn/trunk@86 a35146cb-b0d3-3b4e-005d-c28991089fd7

--- forum.class.php
+++ forum.class.php
@@ -23,6 +23,9 @@
         function moduleInstall() {
             $oModuleController = &getController('module');
             $oModuleModel = &getModel('module');
+            $oDocumentController = &getController('document');
+            $oCommentController = &getController('comment');
+            
 
             // insert triggers 
             $oModuleController->insertTrigger('member.getMemberMenu', 'forum', 'controller', 'triggerMemberMenu', 'after');
@@ -33,19 +36,36 @@
             $oModuleController->insertTrigger('forum.dispForumAdditionSetup', 'editor', 'view', 'triggerDispEditorAdditionSetup', 'before');
             
             // Create the basic forum
+            $args->mid = 'forum';
+            $args->module = 'forum';
+            $args->browser_title = 'XpressEngine';
+            $args->skin = 'dtheme';
             $args->site_srl = 0;
-            $output = executeQuery('module.getSite', $args);
+            $insert_module = $oModuleController->insertModule($args);
+            //create demo category
+            $obj->module_srl = $insert_module->get('module_srl');
+            $obj->title = 'Demo Category';
+            $insert_category = $oDocumentController->insertCategory($obj);
+            //create demo thread
+            $document_args->title = 'Demo Thread';
+            $document_args->content = 'This is a a thread example';
+            $document_args->module_srl = $insert_module->get('module_srl');
+            $document_args->category_srl = $insert_category->get('category_srl');
+            $document_args->comment_status = 'ALLOW';
+            $insert_document = $oDocumentController->insertDocument($document_args);
+            //create demo comment
+            $comment_args->content = '<p>This is a comment example</p>';
+            $comment_args->document_srl = $insert_document->get('document_srl');
+            $comment_args->module_srl = $insert_module->get('module_srl');
+            $comment_args->status = '1';
+            $insert_comment = $oCommentController->insertComment($comment_args);
+            //add basic forum to be index module if index module doesn't exist
+            $argx->site_srl = 0;
+            $output = executeQuery('module.getSite', $argx);
             if(!$output->data->index_module_srl) {
-                $args->mid = 'forum';
-                $args->module = 'forum';
-                $args->browser_title = 'XpressEngine';
-                $args->skin = 'dtheme';
-                $args->site_srl = 0;
-                $output = $oModuleController->insertModule($args);
-                $module_srl = $output->get('module_srl');
+                $module_srl = $insert_module->get('module_srl');
                 $site_args->site_srl = 0;
                 $site_args->index_module_srl = $module_srl;
-                $oModuleController = &getController('module');
                 $oModuleController->updateSite($site_args);
             }
 
@@ -75,21 +95,27 @@
         function moduleUpdate() {
             $oModuleModel = &getModel('module');
             $oModuleController = &getController('module');
-
-            // updates all the necessary tables in order for the forum to work properly
-            if(!$oModuleModel->getTrigger('member.getMemberMenu', 'forum', 'controller', 'triggerMemberMenu', 'after'))
-                $oModuleController->insertTrigger('member.getMemberMenu', 'forum', 'controller', 'triggerMemberMenu', 'after');
-            if(!$oModuleModel->getTrigger('forum.dispForumCommentSetup', 'comment', 'view', 'triggerDispCommentAdditionSetup', 'before'))
-            	$oModuleController->insertTrigger('forum.dispForumCommentSetup', 'comment', 'view', 'triggerDispCommentAdditionSetup', 'before');
-			if(!$oModuleModel->getTrigger('forum.dispForumAdditionSetup', 'document', 'view', 'triggerDispDocumentAdditionSetup', 'before'))
-            	$oModuleController->insertTrigger('forum.dispForumAdditionSetup', 'document', 'view', 'triggerDispDocumentAdditionSetup', 'before');
-            if(!$oModuleModel->getTrigger('forum.dispForumAdditionSetup', 'file', 'view', 'triggerDispFileAdditionSetup', 'before'))
-            	$oModuleController->insertTrigger('forum.dispForumAdditionSetup', 'file', 'view', 'triggerDispFileAdditionSetup', 'before');
-            if(!$oModuleModel->getTrigger('forum.dispForumAdditionSetup', 'point', 'view', 'triggerDispPointAdditionSetup', 'before'))
-            	$oModuleController->insertTrigger('forum.dispForumAdditionSetup', 'point', 'view', 'triggerDispPointAdditionSetup', 'before');
-            if(!$oModuleModel->getTrigger('forum.dispForumAdditionSetup', 'editor', 'view', 'triggerDispEditorAdditionSetup', 'before'))
-            	$oModuleController->insertTrigger('forum.dispForumAdditionSetup', 'editor', 'view', 'triggerDispEditorAdditionSetup', 'before');
-            	
+            
+            $module_config = $oModuleModel->getModuleConfig('forum');
+            if(!$module_config->first_install){
+            	$this->moduleInstall();
+            	$module_config->first_install = 'Y';
+            	$oModuleController->insertModuleConfig('forum',$module_config);
+            }else{
+            	// updates all the necessary tables in order for the forum to work properly
+	            if(!$oModuleModel->getTrigger('member.getMemberMenu', 'forum', 'controller', 'triggerMemberMenu', 'after'))
+	                $oModuleController->insertTrigger('member.getMemberMenu', 'forum', 'controller', 'triggerMemberMenu', 'after');
+	            if(!$oModuleModel->getTrigger('forum.dispForumCommentSetup', 'comment', 'view', 'triggerDispCommentAdditionSetup', 'before'))
+	            	$oModuleController->insertTrigger('forum.dispForumCommentSetup', 'comment', 'view', 'triggerDispCommentAdditionSetup', 'before');
+				if(!$oModuleModel->getTrigger('forum.dispForumAdditionSetup', 'document', 'view', 'triggerDispDocumentAdditionSetup', 'before'))
+	            	$oModuleController->insertTrigger('forum.dispForumAdditionSetup', 'document', 'view', 'triggerDispDocumentAdditionSetup', 'before');
+	            if(!$oModuleModel->getTrigger('forum.dispForumAdditionSetup', 'file', 'view', 'triggerDispFileAdditionSetup', 'before'))
+	            	$oModuleController->insertTrigger('forum.dispForumAdditionSetup', 'file', 'view', 'triggerDispFileAdditionSetup', 'before');
+	            if(!$oModuleModel->getTrigger('forum.dispForumAdditionSetup', 'point', 'view', 'triggerDispPointAdditionSetup', 'before'))
+	            	$oModuleController->insertTrigger('forum.dispForumAdditionSetup', 'point', 'view', 'triggerDispPointAdditionSetup', 'before');
+	            if(!$oModuleModel->getTrigger('forum.dispForumAdditionSetup', 'editor', 'view', 'triggerDispEditorAdditionSetup', 'before'))
+	            	$oModuleController->insertTrigger('forum.dispForumAdditionSetup', 'editor', 'view', 'triggerDispEditorAdditionSetup', 'before');
+            }
             return new Object(0, 'success_updated');
         }
 		/**
